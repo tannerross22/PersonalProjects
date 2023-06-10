98320eb8aa495ba024e0589edc45af1a106bf7c0
tannerross22 <tannerross22@yahoo.com>
2023-06-09 22:26:42 -0500

    corrected math, fixed everything except sticking

--- quadratic_old.py
+++ quadratic_old.py
@@ -3,16 +3,17 @@ import numpy as np
 import scipy.optimize as opt
 from matplotlib.animation import FuncAnimation
 
+
 # Constants
 gravity = 9.81
 time_step = 0.01
-duration = 10.0
+duration = 100
 
 # Simulate the ball's motion
 position = np.array([0.0, 2])
-velocity = np.array([0, 0])
+velocity = np.array([8, 0])
 time = 0.0
-bounce = 0.90
+bounce = 0.80
 time_values = []
 position_values = []
 x = np.linspace(-4, 4, 100)
@@ -20,6 +21,9 @@ x = np.linspace(-4, 4, 100)
 
 def f(x):
     return x**2
+
+
+
 
 
 def reflect_velocity(position, velocity):
@@ -36,8 +40,12 @@ def reflect_velocity(position, velocity):
     # Calculate the derivative of the quadratic equation at the ball's x-coordinate
     derivative = 2 * position_x
 
+    # Calculate the tangent vector of the wall at the point of contact
+    tangent_vector = np.array([1, derivative])
+    tangent_vector = tangent_vector / np.linalg.norm(tangent_vector)
+
     # Calculate the normal vector of the wall at the point of contact
-    normal_vector = np.array([-1, derivative])
+    normal_vector = np.array([-derivative, 1])
     normal_vector = normal_vector / np.linalg.norm(normal_vector)
 
     # Calculate the component of the ball's velocity in the direction perpendicular to the wall
@@ -50,6 +58,8 @@ def reflect_velocity(position, velocity):
     new_vel = velocity - 2 * VnN
 
     return new_vel
+
+
 def snap_to_quadratic(position):
     # Quadratic equation: y = f(x)
     def f(x):
@@ -81,11 +91,6 @@ while time <= duration:
     if position[1] <= position[0]**2:
         position = snap_to_quadratic(position)
         velocity = reflect_velocity(position, velocity * bounce)
-        if abs(velocity[1]) < 0.1:
-            velocity[1] = 0.0
-
-            # Move the ball slightly above the curve to avoid sticking
-        position[1] = f(position[0]) + 0.01
 
     # Increment the time
     time += time_step
@@ -99,17 +104,12 @@ line, = ax.plot([], [], 'o-', lw=2)
 func_line, = ax.plot(x, x ** 2, 'r-', lw=2)
 
 
-def init():
-    line.set_data([], [])
-    return line,
-
-
 def update(frame):
-    line.set_data(position_values[frame, 0], position_values[frame, 1])
+    line.set_data([position_values[frame, 0]], [position_values[frame, 1]])
     return line,
 
 
-ani = FuncAnimation(fig, update, frames=len(time_values), interval=10, init_func=init, blit=True)
+ani = FuncAnimation(fig, update, frames=len(time_values), interval=time_step*1000)
 
 
 # Set the x and y limits of the plot
